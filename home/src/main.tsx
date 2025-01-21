de231e128a67fdefeccd3f40c10b0cf8032b535c
Diego Coronado <dcoronado@able.co>
2025-01-21 22:06:26 +0900

    add home page to home proj

--- home/src/main.tsx
+++ home/src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import "./index.css";
-import App from "./App.tsx";
+import Home from "./pages/Home.tsx";
 import Layout from "./components/Layout.tsx";
 import { NotFound } from "./components/NotFound.tsx";
 import { SidebarProvider } from "./components/ui/sidebar.tsx";
@@ -18,7 +18,7 @@ createRoot(document.getElementById("root")!).render(
             <div className="flex flex-col flex-1 h-full overflow-hidden">
               <Routes>
                 <Route path="/" element={<Layout />}>
-                  <Route index element={<App />} />
+                  <Route index element={<Home />} />
                 </Route>
                 <Route path="*" element={<NotFound />} />
               </Routes>
