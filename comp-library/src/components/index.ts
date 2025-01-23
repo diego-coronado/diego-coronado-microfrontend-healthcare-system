248ba9f89b552967b03c80e55d7301f72b8181e4
Diego Coronado <dcoronado@able.co>
2025-01-23 12:41:56 +0900

    add component library proj

---
+++ comp-library/src/components/index.ts
@@ -0,0 +1,10 @@
+export { ThemeProvider } from "./theme-provider";
+export { ThemeToggle } from "./ThemeToggle";
+export {
+  Card,
+  CardHeader,
+  CardFooter,
+  CardTitle,
+  CardDescription,
+  CardContent,
+} from "./ui/card";
---
+++ comp-library/vite.config.ts
@@ -0,0 +1,40 @@
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import federation from "@originjs/vite-plugin-federation";
+
+export default defineConfig({
+  plugins: [
+    react(),
+    federation({
+      name: "hs-comp-library",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./components": "./src/components/index.ts"
+      },
+      shared: {
+        react: { version: '*' },
+        'react-dom': { version: '*' }
+      }
+    }),
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    modulePreload: false,
+    target: "esnext",
+    minify: false,
+    cssCodeSplit: false,
+    rollupOptions: {
+      output: {
+        inlineDynamicImports: false,
+      },
+    },
+  },
+  server: {
+    port: 5175,
+  },
+});
